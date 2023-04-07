11f28c0e29c8db40cacefb1256b74aed97244dd8
KyrianU <kyrian.uwaegbulem@gmail.com>
2023-04-07 18:28:05 +0000

    added product detail functionality and css changes

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -14,5 +14,19 @@ def all_products(request):
 
     }
     template = 'products/products.html'
+
+    return render(request, template, context)
+
+
+def product_detail(request, product_id):
+    """A view to show individual product details"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+
+    }
+    template = 'products/product_detail.html'
 
     return render(request, template, context)
